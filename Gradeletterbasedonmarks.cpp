6aa530657d3d5ff0b04d49b3f682d672c4441b02
J R Deva Dattan <jrdevadattan2006@gmail.com>
2026-02-02 09:23:17 +0530

    Create Gradeletterbasedonmarks.cpp

---
+++ Gradeletterbasedonmarks.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cout << "marks obtained = ";
+    cin >> marks;
+
+    if(marks>=90 && marks<=100) {
+        cout << "Grade O";
+    }else if(marks>=80 && marks<90) {
+        cout << "Grade A";
+    }else if(marks>=70 && marks<80) {
+        cout << "Grade B";  
+    }else if(marks>=60 && marks<70) {
+        cout << "Grade C";
+    }else if(marks>=50 && marks<60) {
+        cout << "Grade D";
+    }else if(marks>=40 && marks<50) {
+        cout << "Grade E";
+    }else if(marks>=0 && marks<40) {
+        cout << "Grade F";
+    }else {
+        cout << "invalid marks";
+    }
+    return 0;
+}
+
