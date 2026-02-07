4e71ad399f8aedd7b137986cbb0c646f94c86342
J R Deva Dattan <jrdevadattan2006@gmail.com>
2026-02-07 09:14:17 +0530

    Create FactorialofaNumber.cpp

---
+++ FactorialofaNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int fact = 1;
+    cout<<"Enter n: ";
+    cin>>n;
+    for (int i=1; i <=n; i++){
+        fact*= i;
+    }
+    cout<<"Factorial is "<<fact;
+}
