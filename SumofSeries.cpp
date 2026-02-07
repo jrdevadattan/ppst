538a22caa7d3f4cc58f3ef5e6296e1a317084902
J R Deva Dattan <jrdevadattan2006@gmail.com>
2026-02-07 09:11:32 +0530

    Fix loop to calculate sum starting from 1
    Corrected loop initialization to start from 1 instead of 0.

--- SumofSeries.cpp
+++ SumofSeries.cpp
@@ -5,7 +5,7 @@ int main() {
     int n,sum;
     cout<<"Enter n: ";
     cin>>n;
-    for (int i=0; i <=n; i++){
+    for (int i=1; i <=n; i++){
         sum+=i;
     }
     cout<<"Sum of series is "<<sum;
